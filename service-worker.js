a4140bfd0be2967ddde281298d27e5aa32d97fe5
Rajendra <rajendra.prasad@tothenew.com>
2017-05-18 23:00:41 +0530

    service worker implementaition

--- service-worker.js
+++ service-worker.js
@@ -94,9 +94,35 @@
 	self.addEventListener("fetch",function(event){
 		event.respondWith(caches.match(event.request)
 			.then(function(response){
-				return response;
+				return response || fetchAndCache(event.request);
 			})
 		);
 	});
 
+
+	fetchAndCache(url){
+		return fetch(url).then(function(response){
+			if(!response.ok){
+				 throw Error(response.statusText);
+			}
+			return caches.open(cacheName)
+			.then(function(cache){
+				cache.put(url,response.clone());
+				return response;
+			})
+		})
+		.catch(function(error){
+			console.log(error);
+		})
+	}
+
 }());
+
+
+
+
+
+
+
+
+
