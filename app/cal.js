45b04e63c832d97d56e7cdb9b98357d624baf652
Rajendra <rajendra.prasad@tothenew.com>
2017-10-26 11:30:49 +0530

    karma added and service unit testing

--- app/cal.js
+++ app/cal.js
@@ -4,3 +4,79 @@ var calculator = {
 	},
 	sub:(x,y)=>x-y
 }
+
+
+var MyApp = angular.module('MyApp', []);
+
+//Writing a filter
+MyApp.filter('reverse',[function(){
+	return function(string){
+		return string.split("").reverse().join("");
+	}
+}]);
+
+
+//Writing a controller
+MyApp.controller('CalculateController', ['$scope', function($scope){
+	$scope.add = function(){
+		$scope.sum = $scope.num1 + $scope.num2;
+	}
+}]);
+
+//Writing a service
+MyApp.factory('PlayerLocalApi', [function(){
+	
+	var data = [{
+		"id":1,
+		"name":"rajendra prasad",
+		"age":28
+	},{
+		"id":2,
+		"name":"sachin",
+		"age":22
+	}];
+
+	var PlayerLocalApi = {};
+	PlayerLocalApi.getPlayers = function(){
+		return data;
+	}
+
+	return PlayerLocalApi;
+
+}]);
+
+
+ MyApp.factory('playerApi',['$http', function($http){
+ 	 
+ 	var dataUrl = "https://api.myjson.com/bins/ijeaf";
+
+ 	var PlayerLocalApi = {};
+
+ 	PlayerLocalApi.getPlayers = function(){
+ 		return $http.get(dataUrl);
+ 	}
+
+ 	return PlayerLocalApi;
+
+ }]);
+
+
+// MyApp.factory('PlayerLocalApi',function(){
+// 	var data = [{
+// 			"id":1,
+// 			"name":"rajendra prasad",
+// 			"age":28
+// 		},{
+// 			"id":2,
+// 			"name":"sachin",
+// 			"age":22
+// 		}];
+// 		var PlayerLocalApi = {};
+
+// 		PlayerLocalApi.getPlayers = function(){
+// 			return data;
+// 		}
+
+// 		return PlayerLocalApi;
+// })
+
--- test/calTest.js
+++ test/calTest.js
@@ -13,3 +13,109 @@ describe('calculator tests',function(){
 		});
 	})	
 });
+
+
+describe('App Tests',()=>{
+	beforeEach(module('MyApp'));
+
+	describe('reversestring',function(){
+		var reverse;
+
+		beforeEach(inject(function($filter){
+			reverse = $filter('reverse',{});
+		}))
+
+		it('should reverse a string',()=>{
+			expect(reverse('rahil')).toBe('lihar');
+			expect(reverse('test')).toBe('tset');
+		})
+
+	});
+
+
+	describe("add controller",function(){
+		var $controller;
+
+		beforeEach(inject(function(_$controller_){
+			$controller = _$controller_;
+		}));
+
+		it('1 + 1 shoule equal 2',function(){
+			var $scope = {};
+			var controller = $controller('CalculateController',{$scope});
+			$scope.num1 = 1;
+			$scope.num2 = 5;
+
+			$scope.add(); 
+			expect($scope.sum).toBe(6);
+		});
+	});
+
+
+	describe("playerservicetest",function(){
+		var data = [{
+			"id":1,
+			"name":"rajendra prasad",
+			"age":28
+		},{
+			"id":2,
+			"name":"sachin",
+			"age":22
+		}];
+
+		var PlayerLocalApi;
+
+		beforeEach(inject(function( _PlayerLocalApi_ ){
+			PlayerLocalApi = _PlayerLocalApi_;
+		}));
+
+		it("should return search player data",function(){
+			expect(PlayerLocalApi.getPlayers()).toEqual(data);
+		});
+	});
+
+
+
+	describe('playerservicetestwithhttp',()=>{
+		var playerApi,httpBackend,$scope,$http;
+
+		beforeEach(inject((_$httpBackend_,_playerApi_,$rootScope,_$http_)=>{
+			playerApi = _playerApi_;
+			httpBackend = _$httpBackend_;
+			$scope = $rootScope.$new();       
+			$http = _$http_;
+
+		}));
+
+		 
+		it("service test",function(){
+			var returnData = {};
+			httpBackend.whenGET("https://api.myjson.com/bins/ijeaf").respond(returnData);
+
+
+			var returnedPromise = playerApi.getPlayers();
+			var result;
+
+ 
+			returnedPromise.then(function(response){
+				result = response.data;
+			});
+
+			httpBackend.flush();
+			$scope.$apply();
+
+			console.log("returnedPromise..",returnData,result);
+
+			expect(result).toEqual(returnData);
+		});
+	});
+
+
+});
+
+
+
+
+
+
+
